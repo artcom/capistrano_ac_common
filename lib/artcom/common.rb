0bdff11d4c90811922ff810e531ffd576e6f02fc
Valentin Schunack <valentin.schunack@artcom.de>
2014-06-27 12:01:12 +0200

    changed namespace

--- lib/artcom/common.rb
+++ lib/artcom/common.rb
@@ -4,7 +4,7 @@ def _cset(name, *args, &block)
   end
 end
 
-namespace :common_deploy do
+namespace :ac_common_deploy do
   task :fix_permissions do
     sudo "chown -R #{user}:users #{deploy_to}"
   end
