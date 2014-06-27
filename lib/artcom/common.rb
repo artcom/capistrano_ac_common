15e4d8ebf97c0eaace4a52ddf92f029273940c40
Valentin Schunack <valentin.schunack@artcom.de>
2014-06-27 13:03:25 +0200

    changed namespace

--- lib/artcom/common.rb
+++ lib/artcom/common.rb
@@ -20,16 +20,16 @@ configuration.load do
 
 	namespace :ac_common_deploy do
 
-		# fixes annoying habit of capistrano to create root owned project directory
-		# but without use_sudo we cannot create the project directory since
-		# the runner and user are different.
-		# use as:
-		# after 'deploy:setup', 'misc:fix_permissions'     desc "setup directory structure"
+	# fixes annoying habit of capistrano to create root owned project directory
+	# but without use_sudo we cannot create the project directory since
+	# the runner and user are different.
+	# use as:
+	# after 'deploy:setup', 'misc:fix_permissions'     desc "setup directory structure"
 
 
-	    desc "set all file permission to group: users"
-		  task :fix_permissions do
-		    sudo "chown -R #{user}:users #{deploy_to}"
-		  end
-		end
+    desc "set all file permission to group: users"
+	  task :fix_permissions do
+	    sudo "chown -R #{user}:users #{deploy_to}"
+	  end
+	end
 end
